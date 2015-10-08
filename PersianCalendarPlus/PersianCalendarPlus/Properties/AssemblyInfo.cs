e03cc81bb6d66e0c0e6db69bce11a0a5156c7bed
HamidMosalla <Xellarix@gmail.com>
2015-10-08 18:56:24 +0330

    some changes to assemblyinfo file.

--- PersianCalendarPlus/PersianCalendarPlus/Properties/AssemblyInfo.cs
+++ PersianCalendarPlus/PersianCalendarPlus/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PersianCalendarPlus")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("PersianCalendarPlus is a simple wrapper around System.Globalization.PersianCalendar providing extra functionality.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PersianCalendarPlus")]
